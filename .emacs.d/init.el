15835559f18de9caa847f592cecf3d517c872896
Ray Suliteanu <raysuliteanu@gmail.com>
2024-08-01 16:32:38 -0700

    add slime to emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,10 +9,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(rust-mode)))
+ '(package-selected-packages '(slime rust-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(setq inferior-lisp-program "clisp")
