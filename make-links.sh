448f2d31088dc613a72177dbb8791e0e0941fcb8
Ray Suliteanu <raysuliteanu@gmail.com>
2024-08-18 17:28:19 -0700

    forgot .gitconfig in make-links

--- make-links.sh
+++ make-links.sh
@@ -2,14 +2,17 @@
 
 SRC=$(dirname $0)
 
+# files
 ln -s $SRC/configs/.zshrc $HOME
 ln -s $SRC/configs/.zshenv $HOME
 ln -s $SRC/configs/.zshenv $HOME
+ln -s $SRC/configs/.gitconfig $HOME
 ln -s $SRC/configs/.gitattributes $HOME
 ln -s $SRC/configs/.hgrc $HOME
 ln -s $SRC/configs/.vimrc $HOME
-ln -s $SRC/configs/.emacs.d $HOME
 
+# directories
+ln -s $SRC/configs/.emacs.d $HOME
 ln -s $SRC/configs/.oh-my-zsh/custom $HOME/.oh-my-zsh
 ln -s $SRC/configs/.config/nvim $HOME/.config
 ln -s $SRC/configs/.config/tmux $HOME/.config
