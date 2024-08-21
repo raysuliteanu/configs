bcd623a47c49fb517a27ca7a1871ac6d12dc7e6d
Ray Suliteanu <raysuliteanu@gmail.com>
2024-08-21 15:25:51 -0700

    seems to be a problem with the lint so back it out

--- .config/nvim/lua/kickstart/plugins/lint.lua
+++ .config/nvim/lua/kickstart/plugins/lint.lua
@@ -7,7 +7,6 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint-cli2' },
-        rust = { 'clippy' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
