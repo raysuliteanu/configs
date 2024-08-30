2359e9503f33b3e1d84e34d9b0490720722ec017
Ray Suliteanu <raysuliteanu@gmail.com>
2024-08-30 15:54:05 -0700

    update tmux config

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -1,15 +1,12 @@
 alias a=alias
 
+a zshconfig="vi ~/.zshrc"
+a zalias="vi ~/.oh-my-zsh/custom/aliases.zsh"
+a nvimconfig="vi ~/.config/nvim/init.lua"
+
 a vi=nvim
 a vim=nvim
 a v=nvim
-
-a zshconfig="vi ~/.zshrc"
-
-# edit this file
-a zalias="vi ~/.oh-my-zsh/custom/aliases.zsh"
-
-a nvimconfig="vi ~/.config/nvim/init.lua"
 
 a b=brew
 a bs='b search'
@@ -18,17 +15,27 @@ a bl='b list'
 a bu='b update'
 a bcl='b cleanup'
 a bo='b outdated'
+a ba='brew autoremove'
+a bsl='brew services list'
+a bsoff='brew services stop'
+a bson='brew services start'
+a bsr='brew services run'
+a bup='brew upgrade'
 
 a h=history
-
 a c=clear
 a more='less -F'
 a less='less -F'
-
 #a top='btm --color=nord-light'
 a top=bpytop
 a cat='bat --theme=ansi'
 a curl=curlie
+a e=emacs
+a du=dust
+a fd='fd -H'
+a cd=z
+a t=tmux
+a lg=lazygit
 
 a dps='docker ps'
 
@@ -41,20 +48,7 @@ a lR='ls -R'
 a lrt='ls -lr -snew'
 a lart='ls -lar -snew'
 
-a e=emacs
-
-a du=dust
-
-a f.='find $(pwd)'
-
-a warp='warp-terminal > /dev/null 2>&1'
-
-a find_installed='apt list --installed | grep $*'
+a warp='warp-terminal > /dev/null 2>&1 &'
 
 a kscope='cscope -kd -p 3'
-
-a fd='fd -H'
-
-a cd=z
-a t=tmux
 
