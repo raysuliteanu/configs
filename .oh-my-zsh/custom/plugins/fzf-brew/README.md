ef0009838a2990a26a716d82bf3b4f41e3b4e467
Ray Suliteanu <raysuliteanu@gmail.com>
2024-08-30 18:21:28 -0700

    add some brew+fzf commands as a oh-my-zsh plugin

---
+++ .oh-my-zsh/custom/plugins/fzf-brew/README.md
@@ -0,0 +1,8 @@
+# Custom plugin combining fzf and brew
+From https://github.com/junegunn/fzf/wiki/Examples#homebrew
+
+Commands
+- bip - Brew Install Package
+- bup - Brew Upgrade Package
+- brp - Brew Remove Package
+
---
+++ .oh-my-zsh/custom/plugins/fzf-brew/fzf-brew.plugin.zsh
@@ -0,0 +1,36 @@
+# some functions to combined brew with fzf
+
+# Install (one or multiple) selected application(s)
+# using "brew search" as source input
+# mnemonic [B]rew [I]nstall [P]ackage
+bip() {
+  local inst=$(brew search "$@" | fzf -m)
+
+  if [[ $inst ]]; then
+    for prog in $(echo $inst);
+    do; brew install $prog; done;
+  fi
+}
+
+# Update (one or multiple) selected application(s)
+# mnemonic [B]rew [U]pdate [P]ackage
+bup() {
+  local upd=$(brew leaves | fzf -m)
+
+  if [[ $upd ]]; then
+    for prog in $(echo $upd);
+    do; brew upgrade $prog; done;
+  fi
+}
+
+# Delete (one or multiple) selected application(s)
+# mnemonic [B]rew [R]remove [P]ackage (e.g. uninstall)
+bpp() {
+  local uninst=$(brew leaves | fzf -m)
+
+  if [[ $uninst ]]; then
+    for prog in $(echo $uninst);
+    do; brew uninstall $prog; done;
+  fi
+}
+
