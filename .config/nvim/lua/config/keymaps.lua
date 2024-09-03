6097b948b9448fa7deb541567ece52ca533a65c9
Ray Suliteanu <raysuliteanu@gmail.com>
2024-09-02 18:20:12 -0700

    update keymaps
    add some keymaps I saw from Primeagen
    added some Neotree keymaps that were in my old config
    start working on getting some IntelliJ-style key maps implemented

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,13 +1,36 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "\\", "<cmd>Neotree focus toggle<cr>", { desc = "Toggle Neotree file viewer" })
+vim.keymap.set("n", "\\b", "<cmd>Neotree focus buffers toggle<cr>", { desc = "Toggle Neotree buffer viewer" })
+vim.keymap.set(
+	"n",
+	"\\g",
+	"<cmd>Neotree focus git_status toggle float<cr>",
+	{ desc = "Toggle Neotree git status viewer" }
+)
+
+-- from Primeagen https://raw.githubusercontent.com/ThePrimeagen/init.lua/master/lua/theprimeagen/remap.lua
+-- move selected text up and down like IntelliJ <c-shift-up/down>
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- join line with next but keep cursor at current location
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- when inc scroll, keep cursor centered on the page
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set("n", "Q", "<nop>")
+
+-- end Primeagen
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -16,14 +39,8 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc.
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- Keybinds to make split navigation easier.
--- Use CTRL+<hjkl> to switch between windows
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
 -- Obsidian plugin
+-- change cwd to Obsidian vault
 vim.keymap.set("n", "<leader>Oo", ":cd /home/ray/Documents/Obsidian/Ray<cr>")
 
 -- convert note to template and remove leading white space
@@ -49,5 +66,8 @@ vim.keymap.set("n", "<leader>Oiz", ':Telescope live_grep search_dirs={"/home/ray
 vim.keymap.set("n", "<leader>Ok", ":!mv '%:p' /home/ray/Documents/Obsidian/Ray/Resources/Notes<cr>:bd<cr>")
 -- delete file in current buffer
 vim.keymap.set("n", "<leader>Odd", ":!rm '%:p'<cr>:bd<cr>")
-vim.opt.conceallevel = 1
 -- end Obsidian plugin
+
+-- IntelliJ Keymaps - WIP
+vim.keymap.set("i", "<C-y>", "<Esc>yy", { desc = "Copy ([Y]ank) current line" })
+vim.keymap.set("i", "<C-d>", "<Esc>yypo", { desc = "Duplicate current line" })
